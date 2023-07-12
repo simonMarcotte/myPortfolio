0641209b9ca60caecdbed8e0d79b3788f18dc0e1
Simon Marcotte <marcotte.s2010@gmail.com>
2023-07-12 11:46:37 -0600

    fixed link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer class="bg-gray-300  dark:bg-gray-950 rounded-md m-3 duration-500">
         <div name="contact" class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between duration-500">
-        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">2023 <a href="https://flowbite.com/" class="hover:underline">Simon Marcotte:</a> My Portfolio
+        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">2023 <a href="#" class="hover:underline">Simon Marcotte:</a> My Portfolio
         </span>
         <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
             <li>
