292322b59204169877371ecc3d9a69ca31499a33
Simon M <marcotte.s2010@gmail.com>
2024-07-07 16:32:56 -0700

    scrollbar and mobile upgrades

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import Section from './common/Section';
 import { BsBuilding, BsChevronDown } from 'react-icons/bs';
-
-// Example logos
 import generalFusion from '../assets/logos/generalFusion.png';
 import uofaEngg from '../assets/logos/uofaEngg.png';
 
@@ -21,7 +19,9 @@ const WorkExperience = () => {
             src: generalFusion,
             title: 'General Fusion',
             jobTitle: 'Software/Controls Engineering Co-op',
-            style: 'hover:shadow-red-400',
+            startDate: 'January 2024',
+            endDate: 'August 2024',
+            style: 'hover:shadow-red-500 dark:hover:shadow-red-500',
             details: [
                 'Co-led the design of the core control system using Python to build an OOP-based async TCP server, simultaneously collecting data from 15+ plasma diagnostics 30 times per day, achieving a 1 month advance on release date.',
                 'Led the design of a Python control system for AWG and oscilloscopes to autonomously capture data on plasma reflectometry research equipment through VISA protocol, leveraging OOP for experiment scaling.',
@@ -32,9 +32,11 @@ const WorkExperience = () => {
         {
             id: 2,
             src: uofaEngg,
-            title: 'UofA Faculty of Engineering',
-            jobTitle: 'Program Instructor',
-            style: 'hover:shadow-blue-300',
+            title: 'UAlberta Faculty of Engineering',
+            jobTitle: 'Bridge2Engineering - Program Instructor',
+            startDate: 'May 2023',
+            endDate: 'August 2023',
+            style: 'hover:shadow-blue-400 dark:hover:shadow-blue-400',
             details: [
                 'Led the redesign process for a virtual engineering camp, coordinating tasks among team members, setting deadlines, resulting in a one-month advancement in project schedule, and instructing 45+ students during the camp sessions.',
                 'Developed a Discord bot with Python, asyncio and discord.py, hosted on a GCP VM, facilitating task automation and active engagement across 3 servers and with 300+ students within the official program Discord server.',
@@ -45,26 +47,27 @@ const WorkExperience = () => {
     ];
 
     return (
-        <div name="work-experience" className="flex justify-center">
+        <div name="work-experience" className="flex justify-center px-4">
             <Section title="Work Experience" subtitle="Here are some of the companies I have worked at:" icon={<BsBuilding />}>
-                <div className="w-3/5 grid gap-8 lg:gap-14 lg:grid-cols-1 sm:grid-cols-1">
-                    {companies.map(({ id, src, title, jobTitle, style, details }) => (
-                        <div key={id} className={`shadow-lg hover:scale-105 duration-500 py-4 rounded-lg ${style} shadow-gray-300 dark:shadow-gray-500 overflow-hidden flex flex-col items-center w-full bg-gray-200 dark:bg-zinc-900`}>
+                <div className="w-full sm:w-4/6 grid gap-8 lg:gap-14 lg:grid-cols-1 sm:grid-cols-1">
+                    {companies.map(({ id, src, title, jobTitle, startDate, endDate, style, details }) => (
+                        <div key={id} className={`shadow-lg hover:scale-105 duration-500 py-4 rounded-lg ${style} ease-in-out shadow-gray-300 dark:shadow-gray-500 overflow-hidden flex flex-col w-full bg-gray-200 dark:bg-zinc-900`}>
                             <div className="relative w-full flex flex-col items-center">
                                 <div className="w-full flex items-center justify-between p-4 cursor-pointer" onClick={() => handleToggle(id)}>
                                     <div className="flex items-center">
-                                        <img src={src} alt={title} className="w-32 h-32 mr-4" />
+                                        <img src={src} alt={title} className="w-16 h-16 sm:w-32 sm:h-32 mr-4" />
                                         <div className="flex flex-col">
-                                            <h2 className="text-lg font-bold text-left text-gray-900 dark:text-gray-100 transition-colors duration-500">{title}</h2>
-                                            <h3 className="text-md text-left text-gray-900 dark:text-gray-100 transition-colors duration-500">{jobTitle}</h3>
+                                            <h2 className="text-md sm:text-lg font-bold text-left text-gray-900 dark:text-gray-100 transition-colors duration-500">{title}</h2>
+                                            <h3 className="text-sm sm:text-md font-semibold text-left text-blue-800 dark:text-blue-300 transition-colors duration-500">{jobTitle}</h3>
+                                            <p className="text-xs sm:text-sm text-left text-gray-700 dark:text-gray-300 transition-colors duration-500">{`${startDate} - ${endDate}`}</p>
                                         </div>
                                     </div>
                                     <BsChevronDown className="cursor-pointer" />
                                 </div>
                                 <div
-                                    className={`overflow-hidden transition-max-height duration-500 ease-in-out w-full ${open.includes(id) ? 'max-h-96 opacity-100' : 'max-h-0 opacity-0'}`}
+                                    className={`overflow-hidden duration-500 w-full ${open.includes(id) ? 'max-h-96 opacity-100' : 'max-h-0 opacity-0'}`}
                                 >
-                                    <div className="p-4 bg-gray-200 dark:bg-zinc-900 text-neutral-900 dark:text-gray-100 duration-500">
+                                    <div className="p-4 bg-gray-200 dark:bg-zinc-900 text-neutral-900 dark:text-gray-100 duration-500 max-h-48 overflow-y-scroll scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-200 dark:scrollbar-thumb-gray-700 dark:scrollbar-track-zinc-900">
                                         <ul className="list-disc list-inside text-left">
                                             {details.map((detail, index) => (
                                                 <li key={index}>{detail}</li>
