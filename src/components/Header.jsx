d38bcd738b80c304322d747e307357a0fdb64067
Simon M <marcotte.s2010@gmail.com>
2024-07-07 01:12:57 -0700

    updated for new section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import {MdOutlineNightlightRound, MdWbSunny} from "react-icons/md";
-import {Link} from "react-scroll";
+import { MdOutlineNightlightRound, MdWbSunny } from "react-icons/md";
+import { Link } from "react-scroll";
 
-const Header = ({darkMode, setDarkMode}) => {
+const Header = ({ darkMode, setDarkMode }) => {
 
   const NavButtons = [
     {
@@ -11,42 +11,44 @@ const Header = ({darkMode, setDarkMode}) => {
     },
     {
       id: 2,
-      link: "portfolio"
+      link: "work-experience"
     },
     {
       id: 3,
-      link: "experience"
+      link: "portfolio"
     },
     {
       id: 4,
+      link: "Technologies"
+    },
+    {
+      id: 5,
       link: "contact"
     }
   ];
 
-
   return (
-
     <header className="bg-gray-300 dark:bg-zinc-900 text-zinc-950 dark:text-white rounded-md m-3 sticky top-0 z-50 duration-500">
       <nav className='flex justify-between items-center p-5'>
         <h1 className='text-2xl'>SIMON M.</h1>
 
         <ul className='md:flex'>
-          {NavButtons.map(({id, link}) => (
+          {NavButtons.map(({ id, link }) => (
             <li key={id} className="px-4 cursor-pointer capitalize font-thin hover:scale-110 duration-200">
-              <Link to={link} smooth duration={500}>{link}</Link>
+              <Link to={link} smooth duration={500} offset={-50}>{link}</Link>
             </li>
           ))}
         </ul>
 
-        <div onClick={() => setDarkMode(!darkMode)}> 
+        <div onClick={() => setDarkMode(!darkMode)}>
           {
             darkMode ? (
               <MdWbSunny className="text-2xl cursor-pointer" />
             ) : (
-              <MdOutlineNightlightRound className="text-2xl cursor-pointer"/>
+              <MdOutlineNightlightRound className="text-2xl cursor-pointer" />
             )
           }
-            
+
         </div>
       </nav>
     </header>
