fc4b0154a21ef325795beffe64c318ceb07ef5a8
Simon <54853018+simonMarcotte@users.noreply.github.com>
2025-02-12 12:33:21 -0700

    colour/ui changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,19 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  safelist: [
+    "hover:text-gray-700",
+    "hover:text-blue-500",
+    "hover:text-pink-500",
+    "hover:shadow-yellow-400",
+    "hover:shadow-green-400",
+    "hover:shadow-blue-600",
+    "hover:shadow-blue-300",
+    "hover:shadow-green-700",
+    "hover:shadow-sky-300",
+    "hover:shadow-red-400",
+    "hover:shadow-gray-500",
+  ],
   theme: {
     extend: {},
   },
